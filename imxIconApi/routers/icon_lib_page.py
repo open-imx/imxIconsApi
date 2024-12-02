9f3a0eeaaa6fa50c393bb825370d89c56848f9f0
Hazedd <20663495+Hazedd@users.noreply.github.com>
2024-12-02 21:55:47 +0100

    UI fix (#29)
    * ui fix paths
    
    * 🚀 Version 0.0.4.dev3
    
    * pipeline use hatch version, no bump
    
    ---------
    
    Co-authored-by: Hazedd <hazzdd@no-replay.github.com>

--- imxIconApi/routers/icon_lib_page.py
+++ imxIconApi/routers/icon_lib_page.py
@@ -27,17 +27,18 @@ async def get_svg_content(
 async def render_icons(request: Request, imx_version: ImxVersionEnum):
     data: dict = {}
     for imx_path, icon_entities in ICON_DICT.items():
-        data[imx_path] = {}
-        for icon_entity in icon_entities[imx_version.name]:
-            svg_content = await get_svg_content(
-                IconRequestModel(imx_path=imx_path, properties=icon_entity.properties),
-                imx_version,
-            )
-            encoded_svg = base64.b64encode(svg_content.encode("utf-8")).decode("utf-8")
-            data[imx_path][icon_entity.icon_name] = {
-                "properties": icon_entity.properties,
-                "svg": encoded_svg,
-            }
+        if len(icon_entities[imx_version.name]) != 0:
+            data[imx_path] = {}
+            for icon_entity in icon_entities[imx_version.name]:
+                svg_content = await get_svg_content(
+                    IconRequestModel(imx_path=imx_path, properties=icon_entity.properties),
+                    imx_version,
+                )
+                encoded_svg = base64.b64encode(svg_content.encode("utf-8")).decode("utf-8")
+                data[imx_path][icon_entity.icon_name] = {
+                    "properties": icon_entity.properties,
+                    "svg": encoded_svg,
+                }
     return templates.TemplateResponse(
         "icons.html", {"request": request, "data": data, "imx_version": imx_version}
     )
