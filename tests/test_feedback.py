5da0274e90b5abf5c777bb47eec6af5da3f1f50f
Hazedd <20663495+Hazedd@users.noreply.github.com>
2025-03-06 20:08:00 +0100

    maybe requests does work in azure webapp (#71)
    Co-authored-by: Hazedd <hazzdd@no-replay.github.com>

--- tests/test_feedback.py
+++ tests/test_feedback.py
@@ -1,52 +1,51 @@
-# import pytest
-# from imxIconApi.routers.v1.feedback import router
-# from imxIconApi.routers.v1.feedback import validate_feedback, Feedback, enforce_domain_whitelist
-# from imxIcons.domain.supportedImxVersions import ImxVersionEnum
-# from imxIcons.iconService import IconService
-# from unittest.mock import patch, MagicMock
-#
-#
-# @pytest.fixture
-# def mock_icon_service():
-#     with patch.object(IconService, 'get_all_icons', return_value={"valid_icon": {}}):
-#         yield
-#
-#
-# @pytest.fixture
-# def valid_feedback():
-#     return Feedback(
-#         icon_name="SignalLow",
-#         icon_url="https://open-imx.github.io/imxIcons/generated/SignalLow.svg",
-#         imx_version=ImxVersionEnum.v124.name,
-#         feedback_text="Great icon!"
-#     )
-#
-#
-# def test_valid_feedback(valid_feedback, mock_icon_service):
-#     assert validate_feedback(valid_feedback) is True
-#
-#
-# def test_default_feedback(valid_feedback, mock_icon_service):
-#     invalid_feedback = valid_feedback.model_copy(update={"feedback_text": "string"})
-#     assert validate_feedback(invalid_feedback) is False
-#
-#
-# def test_invalid_icon_name_feedback(valid_feedback, mock_icon_service):
-#     invalid_feedback = valid_feedback.model_copy(update={"icon_name": "InvalidIcon"})
-#     assert validate_feedback(invalid_feedback) is False
-#
-#
-# def test_invalid_imx_version_feedback(valid_feedback, mock_icon_service):
-#     invalid_feedback = valid_feedback.model_copy(update={"imx_version": "v0"})
-#     assert validate_feedback(invalid_feedback) is False
-#
-#
-# def test_invalid_origin_feedback(valid_feedback, mock_icon_service):
-#     invalid_feedback = valid_feedback.model_copy(update={"icon_url": "https://xxxx-imx.github.io/imxIcons/generated/SignalLow.svg"})
-#     assert validate_feedback(invalid_feedback) is False
-#
-#
-# def test_url_feedback(valid_feedback, mock_icon_service):
-#     invalid_feedback = valid_feedback.model_copy(update={"feedback_text": "http:// Great icon!"})
-#     assert validate_feedback(invalid_feedback) is False
-#
+import pytest
+from imxIconApi.routers.v1.feedback import validate_feedback, Feedback
+from imxIcons.domain.supportedImxVersions import ImxVersionEnum
+from imxIcons.iconService import IconService
+from unittest.mock import patch
+
+
+@pytest.fixture
+def mock_icon_service():
+    with patch.object(IconService, 'get_all_icons', return_value={"valid_icon": {}}):
+        yield
+
+
+@pytest.fixture
+def valid_feedback():
+    return Feedback(
+        icon_name="SignalLow",
+        icon_url="https://open-imx.github.io/imxIcons/generated/SignalLow.svg",
+        imx_version=ImxVersionEnum.v124.name,
+        feedback_text="Great icon!"
+    )
+
+
+def test_valid_feedback(valid_feedback, mock_icon_service):
+    assert validate_feedback(valid_feedback) is True
+
+
+def test_default_feedback(valid_feedback, mock_icon_service):
+    invalid_feedback = valid_feedback.model_copy(update={"feedback_text": "string"})
+    assert validate_feedback(invalid_feedback) is False
+
+
+def test_invalid_icon_name_feedback(valid_feedback, mock_icon_service):
+    invalid_feedback = valid_feedback.model_copy(update={"icon_name": "InvalidIcon"})
+    assert validate_feedback(invalid_feedback) is False
+
+
+def test_invalid_imx_version_feedback(valid_feedback, mock_icon_service):
+    invalid_feedback = valid_feedback.model_copy(update={"imx_version": "v0"})
+    assert validate_feedback(invalid_feedback) is False
+
+
+def test_invalid_origin_feedback(valid_feedback, mock_icon_service):
+    invalid_feedback = valid_feedback.model_copy(update={"icon_url": "https://xxxx-imx.github.io/imxIcons/generated/SignalLow.svg"})
+    assert validate_feedback(invalid_feedback) is False
+
+
+def test_url_feedback(valid_feedback, mock_icon_service):
+    invalid_feedback = valid_feedback.model_copy(update={"feedback_text": "http:// Great icon!"})
+    assert validate_feedback(invalid_feedback) is False
+
